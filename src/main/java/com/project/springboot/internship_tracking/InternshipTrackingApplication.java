ea9e71795b390c8dfc140031264ed5426728f4dc
Burak Kizilkaya <kizilkayaburakk@hotmail.com>
2022-09-01 17:41:25 +0300

    setting up springboot project

---
+++ src/main/java/com/project/springboot/internship_tracking/InternshipTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.project.springboot.internship_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springboot/internship_tracking/InternshipTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot.internship_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
